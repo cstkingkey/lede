53cc4b0758d0bb9f2544954a7e462548bccd793c
Felix Fietkau <nbd@nbd.name>
2020-09-05 11:19:11 +0200

    build: fix extreme build system slowdown caused by SOURCE_DATE_EPOCH changes
    Adding inline shell invocations in per-target variables causes them to be
    executed over and over again, which causes a significant slowdown.
    Fix this by evaluating it only once per package directory
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- include/package-ipkg.mk
+++ include/package-ipkg.mk
@@ -178,7 +178,7 @@ $$(call addfield,Depends,$$(Package/$(1)/DEPENDS)
 )$$(call addfield,LicenseFiles,$(LICENSE_FILES)
 )$$(call addfield,Section,$(SECTION)
 )$$(call addfield,Require-User,$(USERID)
-)$$(call addfield,SourceDateEpoch,$(shell $(TOPDIR)/scripts/get_source_date_epoch.sh $(SOURCE))
+)$$(call addfield,SourceDateEpoch,$(PKG_SOURCE_DATE_EPOCH)
 )$(if $(PKG_CPE_ID),CPE-ID: $(PKG_CPE_ID)
 )$(if $(filter hold,$(PKG_FLAGS)),Status: unknown hold not-installed
 )$(if $(filter essential,$(PKG_FLAGS)),Essential: yes
